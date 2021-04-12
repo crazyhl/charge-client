4905ee88cddd63bc599acaa04910061a527490d5
M1racle.Hao <crazyhl@163.com>
2021-04-12 10:10:21 +0800

    整理初步结构

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,19 @@
 <template >
-  <div class="app">
-    <navigator />
-    <router-view> </router-view>
-  </div>
+  <a-layout>
+    <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
+      <navigator />
+    </a-layout-header>
+    <a-layout-content :style="{ padding: '0 50px', marginTop: '64px' }">
+      <router-view> </router-view>
+    </a-layout-content>
+    <a-layout-footer :style="{ textAlign: 'center' }">
+      生活账 ©2021-{{currentYear}} Created by <a href="https://www.cimple.ink" target="_blank">M1ralce.Hao</a>
+    </a-layout-footer>
+  </a-layout>
 </template>
 
 <script lang=ts>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import directive from '/@/directives/sample'
 import Navigator from '/@/components/HomeNavigator.vue'
 
@@ -16,9 +23,31 @@ export default defineComponent({
   },
   directives: {
     directive
+  },
+  setup() {
+    return {
+      currentYear: ref(new Date().getFullYear())
+    }
   }
 })
 </script>
 
 <style>
+.logo {
+  height: 31px;
+  line-height: 31px;
+  color: #fff;
+  margin: 16px 24px 16px 0;
+  float: left;
+}
+.logo h3.ant-typography{
+  color:#fff
+}
+.site-layout .site-layout-background {
+  background: #fff;
+}
+
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
 </style>
--- src/renderer/components/HomeNavigator.vue
+++ src/renderer/components/HomeNavigator.vue
@@ -1,28 +1,34 @@
 <template>
+  <div class="logo">
+    <a-typography-title :level="3">生活账</a-typography-title>
+  </div>
+  <a-menu
+    theme="dark"
+    mode="horizontal"
+    v-model:selectedKeys="selectedKeys"
+    :style="{ lineHeight: '64px' }"
+  >
+    <a-menu-item key="1">
+      <router-link class="clickable" replace to="/"> Basic </router-link>
+    </a-menu-item>
+    <a-menu-item key="2">
+      <router-link class="clickable" replace to="/about"> About </router-link>
+    </a-menu-item>
+  </a-menu>
   <div class="sidebar">
-    <router-link class="clickable" replace to="/"> Basic </router-link>
-    <router-link class="clickable" replace to="/about"> About </router-link>
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  setup() {
+    return {
+      selectedKeys: ref<string[]>(['1'])
+    }
+  }
+})
+</script>
+
 <style scoped>
-.sidebar {
-  display: flex;
-  background: #9E9E9E;
-}
-.sidebar a {
-  flex-grow: 1;
-  color: white;
-  text-decoration: none;
-  padding: 10px;
-  transition-duration: 0.25s;
-}
-
-.sidebar a:hover {
-  background: rgba(0, 0, 0, 0.25);
-}
-
-.sidebar a:last-child {
-  border-right: none;
-}
 </style>
