e9a4fee3406abd28994ee66ca641083c5043a453
M1racle.Hao <crazyhl@163.com>
2021-05-08 16:58:20 +0800

    统计相关准备上

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,72 +1,46 @@
 <template>
-  <div class="container">
-    <div class="card" >
-      <h2>Electron Clipboard API</h2>
-      <p>{{ contentToCopy }}</p>
-      <div class="card clickable" @click="copyToClipboard">Click To Copy</div>
-    </div>
-    <div class="card" >
-      <h2>Electron Shell & Dialog API</h2>
-      <p v-if="filePath"> {{ filePath }} </p>
-      <p v-else style="color: grey"> File path will display here! </p>
-      <div class="flex">
-        <div class="card clickable" @click="pickItem">Click To Pick File</div>
-        <div class="card clickable" @click="showItem">Click To Show File in Directory</div>
-      </div>
-    </div>
-    <div class="card">
-      <h2>Vue reactivity</h2>
-      <p>Click below button to checkout vue reactivity</p>
-      <sum-equation />
-    </div>
-    <div class="card" >
-      <h2>Vuex Store</h2>
-      <p> count: {{ count }} </p>
-      <div class="flex">
-        <div class="card clickable" @click="increment">Click To Increment by Commit</div>
-      </div>
-    </div>
+  <div>
+    选择月份: <a-select
+      v-model:value="selectMonth"
+      style="width: 120px"
+      ref="select"
+      @change="monthSelectorHandleChange"
+    >
+      <a-select-option :value="month" v-for="month in monthList" :key="month">{{month}}</a-select-option>
+    </a-select>
   </div>
 </template>
 
 <script lang=ts>
-import { defineComponent, reactive, toRefs } from 'vue'
-import { useClipboard, useShell, useDialog, useCount } from '../hooks'
-import SumEquation from './SumEquation.vue'
+import { defineComponent, ref } from 'vue'
+import { summaryMonthList } from '../api/statistics'
+import moment from 'moment'
+import 'moment/dist/locale/zh-cn';
 
 export default defineComponent({
   components: {
-    SumEquation
   },
   setup(props, context) {
-    const data = reactive({
-      contentToCopy: 'hello, you will copy/paste this piece of text!',
-      filePath: ''
+    const currentMonth = moment().format("YYYYMM")
+    const monthList = ref<string[]>([currentMonth])
+    const selectMonth = ref(currentMonth)
+    summaryMonthList().then(response => {
+      const responseMonthList = response.data.data
+      responseMonthList.forEach((date: string) => {
+        if(monthList.value.indexOf(date) === -1) {
+          monthList.value.push(date)
+        }
+      });
     })
-    const { showItemInFolder } = useShell()
-    const { write } = useClipboard()
-    const { showOpenDialog } = useDialog()
-    const name = 'abc'
-    function copyToClipboard() {
-      write({ text: data.contentToCopy })
-    }
-    async function pickItem() {
-      const { filePaths } = await showOpenDialog({
-        title: 'Pick the file to show',
-        properties: ['openFile']
-      })
-      data.filePath = filePaths[0] ?? ''
-    }
-    function showItem() {
-      showItemInFolder(data.filePath)
-    }
+
+    const monthSelectorHandleChange = (value: string) => {
+      console.log(`selected ${value}`);
+    };
+
     return {
-      ...toRefs(data),
-      ...useCount(),
-      name,
-      copyToClipboard,
-      showItem,
-      pickItem
+      monthList,
+      selectMonth,
+      monthSelectorHandleChange,
     }
   }
 })
